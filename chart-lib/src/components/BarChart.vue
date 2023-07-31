cbbad7bb34fed21ff5858a5e7ec8576a74381a05
haramsong <alakazam1324@gmail.com>
2023-07-31 17:46:42 +0900

    07_31학습

---
+++ chart-lib/src/components/BarChart.vue
@@ -0,0 +1,33 @@
+<template>
+    <canvas ref="barChart" id="barChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+
+export default {
+    props: ['propsdata'],
+    mounted() {
+        var myChart = new this.$_Chart(this.$refs.barChart, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: this.propsdata,
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            },
+            onClick: function() {
+                this.$emit('refresh:chart');
+            }
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
